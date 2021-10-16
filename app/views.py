d785a91451ed9c5152f3c3c4c6365d1eeb04c415
Soupy710 <swaroopbhat710@gmail.com>
2021-10-16 18:08:41 +0530

    added login etc.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
 from app import app
-from flask import render_template
+from flask import render_template, Blueprint
 import os
+
+#app = Blueprint('app', __name__)
 
 
 @app.route("/")
@@ -12,3 +14,18 @@ def index():
 @app.route("/about")
 def about():
     return "All about Flask"
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    return 'Logout'
