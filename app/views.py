e40cc0fd8af2a247dd227929ed2731757ab848a1
Surya <suryasandeepraj2002@gmail.com>
2021-10-16 18:26:26 +0530

    icvysvdbluowscd2

--- app/views.py
+++ app/views.py
@@ -9,14 +9,3 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/details", methods=["POST","GET"])
-def details():
-    if request.method == "POST":
-        form = request.form
-        print(form)
-    return render_template("details.html")
-
-@app.route("/qrcode")
-def qrcode():
-    print(os.getcwd())
-    return render_template("qrcode.html")
